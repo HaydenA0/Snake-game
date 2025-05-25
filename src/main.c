fbd57568960477fed50e3b90803e41487c90cd21
Hayden0A <aregrag2005@gmail.com>
2025-05-25 06:21:41 +0100

    implemented basic a basic updater, snake and a basic input handler

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include "raylib.h"
 #include <stdbool.h>
 // #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #define FPS 60
@@ -27,6 +28,71 @@ typedef struct {
   int row;
   int col;
 } SnakeSegment;
+
+SnakeSegment *init_snake() {
+  SnakeSegment *snake = malloc(N_COLUMNS * N_LINES * sizeof(SnakeSegment));
+  return snake;
+}
+
+void handle_boundaries(SnakeSegment *snake, int snake_size) {
+  if ((snake[snake_size - 1].row > N_LINES) ||
+      (snake[snake_size - 1].col > N_COLUMNS)) {
+    printf("OUT OF BOUNDARIES\n");
+  }
+}
+
+int start_snake(SnakeSegment *snake) {
+  int snake_size = 2;
+  snake[0].row = 0;
+  snake[0].col = 0;
+  snake[1].row = 0;
+  snake[1].col = 1;
+  return snake_size;
+}
+
+// Dont code, just try to understand what is this (math : movment propagation)
+// Also figure out why tf the snake is being erased at the start
+
+void input_handler(SnakeSegment *snake, int snake_size) {
+  SnakeSegment head = snake[0];
+  for (int i = snake_size - 1; i == 1; i--) {
+    snake[i] = snake[i - 1];
+  }
+  if (IsKeyPressed(KEY_D)) {
+    snake[0].col = head.col + 1;
+    snake[0].row = head.row;
+  }
+  if (IsKeyPressed(KEY_S)) {
+    snake[0].col = head.col;
+    snake[0].row = head.row + 1;
+  }
+  if (IsKeyPressed(KEY_A)) {
+    snake[0].col = head.col - 1;
+    snake[0].row = head.row;
+  }
+  if (IsKeyPressed(KEY_W)) {
+    snake[0].col = head.col;
+    snake[0].row = head.row - 1;
+  }
+}
+
+void test_straight_path(SnakeSegment *snake, int snake_size) {
+  for (int i = 0; i < snake_size; i++) {
+    snake[i].col += 1;
+  }
+}
+void update_grid(Tile **grid, SnakeSegment *snake, int snake_size) {
+  for (int i = 0; i < N_LINES; i++) {
+    for (int j = 0; j < N_COLUMNS; j++) {
+      grid[i][j].snake = false;
+    }
+  }
+  for (int i = 0; i < snake_size; i++) {
+    int row = snake[i].row;
+    int col = snake[i].col;
+    grid[row][col].snake = true;
+  }
+}
 
 Tile **init_grid() {
   Tile **grid = malloc(N_LINES * sizeof(Tile *));
@@ -61,10 +127,15 @@ void run() {
   SetTargetFPS(FPS);
   InitWindow(HIGHT_RESOLUTION, WIDTH_RESOLUTION, "title");
   Tile **grid = init_grid();
-  grid[4][4].snake = true;
-  grid[2][3].food = true;
+  SnakeSegment *snake = init_snake();
+  int snake_size = start_snake(snake);
+  int frames = 0;
+
   while (!WindowShouldClose()) {
     BeginDrawing();
+    frames++;
+    input_handler(snake, snake_size);
+    update_grid(grid, snake, snake_size);
     draw_grid(grid);
     EndDrawing();
   }
